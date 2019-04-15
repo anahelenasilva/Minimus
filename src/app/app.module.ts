f92e4b9382443ff15bd1936cd1f8553c43ef79b6
Ana Helena <anahelenarp@hotmail.com>
2019-04-15 11:19:50 -0300

    add initial structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { WeatherCardComponent } from './ui/weather-card/weather-card.component';
+import { AddCardComponent } from './ui/add-card/add-card.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { AddComponent } from './pages/add/add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WeatherCardComponent,
+    AddCardComponent,
+    SignupComponent,
+    LoginComponent,
+    DetailsComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ui/weather-card/weather-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-card',
+  templateUrl: './weather-card.component.html',
+  styleUrls: ['./weather-card.component.sass']
+})
+export class WeatherCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
